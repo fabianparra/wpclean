8f44a6ff29e4932384c481e1ace40f759bd394fb
fabianparra <fabianmparra@gmail.com>
2015-09-08 10:02:50 -0500

    Basic Structure

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class( $class ); ?>>
