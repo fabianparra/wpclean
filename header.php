9b48f755fccf07582edfe9138409f1932f5f9471
fabianparra <fabianmparra@gmail.com>
2015-09-16 07:58:25 -0500

    Basic Structure 2

--- header.php
+++ header.php
@@ -1,9 +1,17 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
-<?php wp_head(); ?>
-</head>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <title><?php wp_title(); ?></title>
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+        <?php wp_head(); ?>
+    </head>
 
 <body <?php body_class( $class ); ?>>
+    <header>
+        <nav>
+            <?php wp_nav_menu( array( 'theme_location' => 'main' ) ); ?>
+        </nav>
+    </header>
