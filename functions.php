e93985b28b8b820f3b2ff1c4b3fa0e7f072f7435
Fabian Parra <fabianmparra@gmail.com>
2017-08-16 19:49:40 -0500

    customize login title and link

--- functions.php
+++ functions.php
@@ -101,3 +101,13 @@ function my_login_logo() { ?>
     </style>
 <?php }
 add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Your Site Name and Info';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
